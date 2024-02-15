fe69c80196d6c160018f0a3938d95942b0d77089
Pedro Henrique Ribeiro <127356379+class-pedro@users.noreply.github.com>
2024-02-14 21:35:40 -0300

    Att

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,66 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import "./header.css"
+import { FaMoon } from "react-icons/fa";
+import { FaSun } from "react-icons/fa";
+import desktopWinter from "../../assets/images/desktop-winter-img.jpg";
 
 const HeaderContainer = styled.header`
-    background-color: blue;
+    background: url(${desktopWinter}) no-repeat center center;
+    width: 100vw;
+    height: 60vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 `
+
+const Hour = styled.span`
+    color: white;
+    font-size: 10em;
+    font-weight: 500;
+`
+
+const TextContainer = styled.div`
+    display: flex;
+    color: white;
+    font-size: 2em;
+    align-items: center;
+    justify-content: center;
+`
+
+const Greeting = styled.h1`
+    color: white;
+    font-size: 1.35em;
+    font-weight: 300;
+    text-align: start;
+    margin: 0 0 0 10px;
+`
+
+const newDate = new Date();
+const currentHour = newDate.toLocaleTimeString("pt-BR", {
+    timeStyle: "short",       //Serão retornado apenas horas e minutos.  
+    hour12: false,            //Formato de 24h, suprimindo sufixos AM e PM.
+    numberingSystem: "latn"   //Resulado em algarismos indo-arábicos.
+  });
+const handleGreeting = currentHour >= 5 && currentHour <= 12 ? 'morning' : currentHour >= 12 && currentHour <= 18 ? 'afternoon' : 'evening';
+const moonDisplay = currentHour >= 5 && currentHour < 18 ? 'moon__icon d_none' : 'moon__icon';
+const sunDisplay = currentHour >= 5 && currentHour < 18 ? 'sun__icon' : 'sun__icon  d_none';
+
 
 function Header() {
     return (
         <HeaderContainer>
-            React Clock With Weather
+            <TextContainer>
+                <FaMoon className={moonDisplay} />
+                <FaSun className={sunDisplay} />
+                <Greeting>
+                    Good {handleGreeting}!
+                </Greeting>
+            </TextContainer>
+            <Hour>{currentHour}</Hour>
         </HeaderContainer>
     )
 }
 
-export default Header
+export default Header;
